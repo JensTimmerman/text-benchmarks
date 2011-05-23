c6b766efeeef6c982795890222b5e6086a08d5e9
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-23 09:43:25 +0200

    Port DecodeUtf8 benchmark

--- src/Data/Text/Benchmarks/Micro.hs
+++ src/Data/Text/Benchmarks/Micro.hs
@@ -8,6 +8,7 @@ import Criterion.Main (Benchmark, defaultMain)
 
 import qualified Data.Text.Benchmarks.Micro.Builder as Builder
 import qualified Data.Text.Benchmarks.Micro.CaseMap as CaseMap
+import qualified Data.Text.Benchmarks.Micro.DecodeUtf8 as DecodeUtf8
 import qualified Data.Text.Benchmarks.Micro.FileRead as FileRead
 import qualified Data.Text.Benchmarks.Micro.StripBrackets as StripBrackets
 import qualified Data.Text.Benchmarks.Micro.WordCount as WordCount
@@ -19,6 +20,7 @@ benchmarks :: [Benchmark]
 benchmarks =
     [ Builder.benchmark
     , CaseMap.benchmark "data/russian.txt"
+    , DecodeUtf8.benchmark "data/russian.txt"
     , FileRead.benchmark "data/russian.txt"
     , StripBrackets.benchmark "data/russian.txt"
     , WordCount.benchmark "data/russian.txt"
