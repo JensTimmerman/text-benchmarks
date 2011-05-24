ff6510a8b30c02af66f3480a933d8b1e92516706
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-24 21:20:51 +0200

    Fix bug (need to call total on Tms object)

--- ruby/utils.rb
+++ ruby/utils.rb
@@ -4,8 +4,10 @@ def benchmark(&block)
   runs = 100
   total = 0
 
-  runs.times do 
-    total += Benchmark.measure(&block) 
+  runs.times do |i|
+    result = Benchmark.measure(&block).total
+    $stderr.puts "Run #{i}: #{result}"
+    total += result
   end
 
   total / runs 
