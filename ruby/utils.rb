28b705f5e8e303abec10549a2319c884c93d45e2
Toon Willems <willemstoon@gmail.com>
2011-05-24 11:56:26 -0700

    Use 'benchmark' for benchmarking (duh).

--- ruby/utils.rb
+++ ruby/utils.rb
@@ -1,19 +1,11 @@
-def benchmark_once
-  start = Time.now
-  yield
-  stop = Time.now
-  stop - start
-end
+require 'benchmark'
 
 def benchmark(&b)
   runs = 100
-  total = 0.0
-  runs.times do |i|
-    result = benchmark_once(&b)
-    STDERR.puts "Run #{i}: #{result}"
-    total += result
+  
+  Benchmark.bmbm do |x|
+    x.report { runs.times { b.call } }
   end
-  total / runs
 end
 
 def with_utf8_file(filename)
