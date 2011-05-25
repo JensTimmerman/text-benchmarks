4027d453111aeed2c54b2f87f717d93ce5b46a02
JensTimmerman <jens.timmerman@gmail.com>
2011-05-25 09:23:27 -0700

    using functools.partial compiles new function with fixed arguments, so not fair for benchmarking, using lambda here again

--- python/sort.py
+++ python/sort.py
@@ -9,9 +9,7 @@ def sort(string):
     return '\n'.join(lines)
 
 for f in sys.argv[1:]:
-    t = utils.benchmark(functools.partial(
-                    			sys.stdout.write,
-                    				utils.with_utf8_file(f,sort).encode('utf-8')
-                    			)
-                    	)
+    t = utils.benchmark(lambda: sys.stdout.write(
+                    				utils.with_utf8_file(f,sort).encode('utf-8'))
+                    				)
     sys.stderr.write('{0}: {1}\n'.format(f, t))
