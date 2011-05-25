f7475242e34c1883dccd7995697a3a234c2b1d74
JensTimmerman <jens.timmerman@gmail.com>
2011-05-25 08:56:54 -0700

    don't use lambda if not needed , + use functools.partial instead of lambda (100x faster in my benchmarks)

--- python/sort.py
+++ python/sort.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import utils, sys
+import functools
 
 def sort(string):
-    lines = string.split('\n')
+    lines = string.splitlines()
     lines.sort()
     return '\n'.join(lines)
 
 for f in sys.argv[1:]:
-    t = utils.benchmark(lambda:
-            utils.with_utf8_file(f, lambda c:
-                    sys.stdout.write(sort(c).encode('utf-8'))))
+    t = utils.benchmark(functools.partial(
+                    			sys.stdout.write,
+                    				utils.with_utf8_file(f,sort).encode('utf-8')
+                    			)
+                    	)
     sys.stderr.write('{0}: {1}\n'.format(f, t))
